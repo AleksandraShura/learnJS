aa889443c4180c0409eac21b69af9a2f690a6f8d
Kooo9058 <lennon199526revoldredovn>
2022-04-30 13:37:48 +0300

    comment-some-code

--- my_script.js
+++ my_script.js
@@ -1,44 +1,45 @@
 "use scrict";
 
-let input_array = document.getElementById('input_array');
-let text = document.getElementById('text');
-let button1 = document.getElementById('button1');
-let button2 = document.getElementById('button2');
+// let input_array = document.getElementById('input_array');
+// let text = document.getElementById('text');
+// let button1 = document.getElementById('button1');
+// let button2 = document.getElementById('button2');
 
-const separator = ","
+// const separator = ","
 
-button2.addEventListener("click", () => {
-    if (input_array.value.length === 0) {
-        alert('Введите, пожалуйста, массив чисел!')
-    } else {
-    let arrayFromInput = stringsToArray(input_array.value, separator)
-    let oddNumsArr = oddNumbers(arrayFromInput)
-    text.textContent = oddNumsArr
-    }
-})
+// button2.addEventListener("click", () => {
+//     if (input_array.value.length === 0) {
+//         alert('Введите, пожалуйста, массив чисел!')
+//     } else {
+//     let arrayFromInput = stringsToArray(input_array.value, separator)
+//     let oddNumsArr = oddNumbers(arrayFromInput)
+//     text.textContent = oddNumsArr
+//     }
+// })
 
-button1.addEventListener("click", () => {
-    if (input_array.value.length === 0) {
-        alert('Введите, пожалуйста, массив чисел!')
-    } else {
-        let arrayFromInput = stringsToArray(input_array.value, separator)
-        let evenNumsArr = evenNumbers(arrayFromInput) 
-        text.textContent = evenNumsArr
-    }
-})
+// button1.addEventListener("click", () => {
+//     if (input_array.value.length === 0) {
+//         alert('Введите, пожалуйста, массив чисел!')
+//     } else {
+//         let arrayFromInput = stringsToArray(input_array.value, separator)
+//         let evenNumsArr = evenNumbers(arrayFromInput) 
+//         text.textContent = evenNumsArr
+//     }
+// })
 
-function evenNumbers(array) {
-    return array.filter(element => element % 2 == 0)
-}
+// function evenNumbers(array) {
+//     return array.filter(element => element % 2 == 0)
+// }
 
-function oddNumbers(array) {
-    return array.filter(element => element % 2 != 0)
-}
+// function oddNumbers(array) {
+//     return array.filter(element => element % 2 != 0)
+// }
 
 function print(message) {
     console.log(`${message}`)
 }
 
-function stringsToArray(strings, separator) {
-    return strings.split(separator)
-}
+// function stringsToArray(strings, separator) {
+//     return strings.split(separator)
+// }
+ghghghgh
