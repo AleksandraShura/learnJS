0f70569a26e79186e58ee4365d06f2ac57384dcf
Kooo9058 <lennon199526revoldredovn>
2022-05-02 20:24:51 +0300

    codewars-test

--- my_script.js
+++ my_script.js
@@ -1,44 +1,50 @@
 "use scrict";
 
-let input_array = document.getElementById('input_array');
-let text = document.getElementById('text');
-let button1 = document.getElementById('button1');
-let button2 = document.getElementById('button2');
+// function makeNegative(num) {
+//     // let negative =  num > 0 ? num * -1 : num
 
-const separator = ","
+//     // if(num > 0) {
+//     //     negative = num * -1   
+//     // } else {
+//     //     negative = num
+//     // }
 
-button2.addEventListener("click", () => {
-    if (input_array.value.length === 0) {
-        alert('Введите, пожалуйста, массив чисел!')
-    } else {
-    let arrayFromInput = stringsToArray(input_array.value, separator)
-    let oddNumsArr = oddNumbers(arrayFromInput)
-    text.textContent = oddNumsArr
-    }
-})
+//     return num > 0 ? num * -1 : num
+// }
+// console.log(makeNegative(8))
 
-button1.addEventListener("click", () => {
-    if (input_array.value.length === 0) {
-        alert('Введите, пожалуйста, массив чисел!')
-    } else {
-        let arrayFromInput = stringsToArray(input_array.value, separator)
-        let evenNumsArr = evenNumbers(arrayFromInput) 
-        text.textContent = evenNumsArr
-    }
-})
 
-function evenNumbers(array) {
-    return array.filter(element => element % 2 == 0)
-}
+// function betterThanAverage(classPoints, yourPoints) {
+//     classPoints.push(yourPoints)
+//     let sumPoints = classPoints.reduce((sum, current) => sum + current)
+//     let middlePoint = sumPoints / classPoints.length
+    
+//     return yourPoints > middlePoint ? true : false
+    
+//     // if( yourPoints > middlePoint ) {
+//     //     return true
+//     // } else {
+//     //     return false
+//     // }
+// }
+// console.log(betterThanAverage([2, 4, 6, 7], 7))
 
-function oddNumbers(array) {
-    return array.filter(element => element % 2 != 0)
-}
+// function makeUpperCase(str) {
+//     return str.toUpperCase()
+// }
+// console.log(makeUpperCase("привет"))
 
-function print(message) {
-    console.log(`${message}`)
-}
-
-function stringsToArray(strings, separator) {
-    return strings.split(separator)
-}
+// function bmi(weight, height) {
+//     let bmi = weight / (height * height)
+   
+//     if(bmi <= 18.5) {
+//         return "Underweight"
+//     } else if(bmi <= 25.0) {
+//         return "Normal"
+//     } else if(bmi <= 30.0) {
+//         return "Overweight"
+//     } else if(bmi > 30) {
+//         return "Obese"
+//     }   
+// }
+// console.log(bmi(71, 1.74))
